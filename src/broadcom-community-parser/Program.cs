ef4215f2f266e960dece47ae52c274d6233bd59c
Alex Hedley <alex.hedley@me.com>
2023-05-21 18:44:30 +0100

    #1 WIP get images

--- src/broadcom-community-parser/Program.cs
+++ src/broadcom-community-parser/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System.Configuration;
+using System.Net;
+using System.Security.Policy;
+using System.Web;
 using System.Xml;
 using System.Xml.Serialization;
 
 using broadcom_community_parser.Models;
+using HtmlAgilityPack;
 
 Console.WriteLine("Hello, World!");
 
@@ -53,13 +57,53 @@ foreach(var document in documents)
 Console.WriteLine(posts.Count());
 
 string outputFolder = ConfigurationManager.AppSettings["outputFolder"];
+// Create Folders?
 
 foreach (var post in posts)
 {
     try
     {
-        var invalids = System.IO.Path.GetInvalidFileNameChars();
+        var invalids = Path.GetInvalidFileNameChars();
         var newName = String.Join("_", post.Name.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+        newName = newName.Trim();
+
+        // Get image src
+        // From String
+        var doc = new HtmlDocument();
+        doc.LoadHtml(post.Description);
+
+        string command = "//img";
+        List<string> listImages = new();
+        foreach (HtmlNode node in doc.DocumentNode.SelectNodes(command))
+        {
+            var src = node.Attributes["src"].Value;
+            Console.WriteLine(src);
+            
+            ////src = HttpUtility.UrlEncode(src);
+            //src = Uri.EscapeDataString(src);
+            //Console.WriteLine(src);
+
+            var imageFileName = Path.GetFileName(src);
+            imageFileName = HttpUtility.UrlEncode(imageFileName);
+            Console.WriteLine(imageFileName);
+            var imagesPath = Path.Combine(outputFolder, "images", imageFileName);
+
+            // Check if image already exists.
+            if (File.Exists(imagesPath))
+            {
+                Console.WriteLine("Image already exists on disk, no need to download");
+            } 
+            else
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri(src), imagesPath); //DownloadFile
+                }
+            }
+
+            // replace src with local for markdown conversion next
+            post.Description = post.Description.Replace(src, Path.Combine("images", imageFileName));
+        }
 
         string fileName = $"{outputFolder}\\{newName}.md";
         var converter = new ReverseMarkdown.Converter();
@@ -71,7 +115,7 @@ foreach (var post in posts)
         //    File.Delete(fileName);
         //}
 
-        // Create a new file     
+        //Create a new file
         using (StreamWriter sw = File.CreateText(fileName))
         {
             //---
